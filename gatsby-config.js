ac726f5e4d02d95faf263cc0afc97b8a01615e54
Erik Sherman <erikkennethsherman@gmail.com>
2021-05-06 08:17:56 -0400

    added styled components, gatsby-source-filesystem, gatsby-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,28 @@ module.exports = {
     siteMetadata: {
         title: 'gatsby-starter',
     },
-    plugins: ['gatsby-plugin-react-helmet'],
+    plugins: [
+        'gatsby-plugin-react-helmet',
+        'gatsby-plugin-styled-components',
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `assets`,
+                path: `${__dirname}/assets`,
+            },
+        },
+        {
+            // See https://www.gatsbyjs.com/plugins/gatsby-plugin-manifest/?=gatsby-plugin-manifest
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `Gatsby Starter`,
+                short_name: `Starter`,
+                start_url: `/`,
+                background_color: `#181618`,
+                theme_color: `#99CCED`,
+                display: `standalone`,
+                icon: `./assets/logo.svg`,
+            },
+        },
+    ],
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,19 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
 const HomePage = () => {
-    return <div>this</div>
+    return (
+        <Container>
+            <h1>starter</h1>
+        </Container>
+    )
 }
 
 export default HomePage
